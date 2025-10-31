088012b6b8fd17ed67a2d057f043f92f1d40026e
Shawon <mdmoinulhossainshawon@gmail.com>
2025-10-31 11:04:49 +0600

    chore: Added some helpers

--- lua/mdtypes.lua
+++ lua/mdtypes.lua
@@ -1,11 +1,175 @@
 local mdtypes = {};
 
+--[[ Gets annotations from `path`. ]]
+---@param path string
+---@return table
+mdtypes._annotations = function (path)
+	---|fS
+
+	local could_open, lines = pcall(vim.fn.readfile, vim.fn.expand("%:h") .. "/" .. path);
+
+	if not could_open then
+		return {};
+	end
+
+	local annotations = {};
+	local buffer = {};
+
+	local is_within_chunk = false;
+
+	local function flush (line)
+		if is_within_chunk and #annotations > 0 then
+			table.insert(buffer, line);
+			vim.list_extend(annotations[#annotations].lines or {}, buffer);
+
+			is_within_chunk = false;
+		end
+
+		buffer = {};
+	end
+
+	for _, line in ipairs(lines) do
+		line = string.gsub(line, "^%s+", "");
+
+		if string.match(line, "^%s*$") then
+			flush();
+		elseif not string.match(line, "^%s*%-%-+") then
+			if #buffer > 0 and ( string.match(line, "(%w+)%s*=%s*function") or string.match(line, "function%s+(%w+)") ) then
+				local name = string.match(line, "(%w+)%s*=%s*function") or string.match(line, "function%s+(%w+)");
+
+				table.insert(annotations, {
+					kind = "funcref",
+					name = name,
+					lines = buffer
+				});
+
+				buffer = {};
+			else
+				flush();
+			end
+		elseif string.match(line, "^%s*%-%-%-+@%w+") then
+			local valid = {
+				"class",
+				"alias",
+			};
+			local kind, name = string.match(line, "^%s*%-%-%-+@(%w+)%s*(%S+)");
+
+			if vim.list_contains(valid, kind) then
+				-- Annotation.
+				flush();
+				table.insert(annotations, {
+					kind = kind,
+					name = name,
+					lines = { line }
+				});
+				is_within_chunk = true;
+			else
+				table.insert(buffer, line);
+			end
+		else
+			table.insert(buffer, line);
+		end
+	end
+
+	flush();
+	return annotations;
+
+	---|fE
+end
+
+mdtypes._function = function (path)
+	---|fS
+
+	local could_open, lines = pcall(vim.fn.readfile, vim.fn.expand("%:h") .. "/" .. path);
+
+	if not could_open then
+		return {};
+	end
+
+	local buf = vim.api.nvim_create_buf(false, true);
+	vim.bo[buf].ft = "lua";
+
+	vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines);
+
+	local annotations = {};
+	local query = vim.treesitter.query.parse("lua", [[
+		(function_declaration
+			name: (_)) @funcdecl
+
+		(function_definition) @funcdef
+	]]);
+
+	local root_parser = vim.treesitter.get_parser(buf);
+
+	if not root_parser then
+		return {};
+	end
+
+	local function remove_leader (entries, row_start)
+		local _lines = {};
+
+		for _, line in ipairs(entries) do
+			table.insert(_lines, string.sub(line, row_start));
+		end
+
+		return _lines;
+	end
+
+	root_parser:parse(true);
+	root_parser:for_each_tree(function (TSTree, LanguageTree)
+		if LanguageTree:lang() == "lua" then
+			for capture_id, capture_node, _, _ in query:iter_captures(TSTree:root(), buf) do
+				local name = query.captures[capture_id];
+
+				if name == "funcdecl" then
+					local R = { capture_node:range() };
+					local flines = vim.api.nvim_buf_get_lines(buf, R[1], R[3] + 1, false);
+
+					local fname_node = capture_node:field("name")[1];
+				vim.print(name);
+
+					table.insert(annotations, {
+						kind = "functdecl",
+						name = fname_node and vim.treesitter.get_node_text(fname_node, buf, {}) or nil,
+
+						lines = remove_leader(flines, #string.match(flines[1] or "", "^%s*"))
+					});
+				elseif name == "funcdef" then
+					local R = { capture_node:range() };
+					local flines = vim.api.nvim_buf_get_lines(buf, R[1], R[3] + 1, false);
+
+					local fname = string.match(flines[1] or "", "(%S+)%s*=%s*function");
+
+					table.insert(annotations, {
+						kind = "functdecl",
+						name = fname,
+
+						lines = remove_leader(flines, #string.match(flines[1] or "", "^%s*"))
+					});
+				end
+			end
+		end
+	end)
+
+	return annotations;
+
+	---|fE
+end
+
+mdtypes._eval = function (expr)
+	return vim.api.nvim_exec2("=" .. expr, { output = true }).output;
+end
+
 mdtypes.get_classes = function (path)
 	local could_open, result = pcall(vim.fn.readfile, vim.fn.expand("%:h") .. "/" .. path);
 
 	if not could_open then
 		result = { result };
 	end
+
+-- vim.print(
+-- 	pcall(mdtypes._eval, 'vim')
+-- );
 
 	local classes = {};
 	local tmp = {};
